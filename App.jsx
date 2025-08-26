64c297bde403c50f6def6a818bf1906358caf6ba
CharanKumarKubireddi <charankubireddi@gmail.com>
2025-08-26 22:34:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./navbar";
+import Details from "./Details.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Details />
+    </>
+  );
+}
+
+export default App;
